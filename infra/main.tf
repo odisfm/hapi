12ac58720b0c69950e04e9ecd06cafc66682224c
odisfm <102568741+odisfm@users.noreply.github.com>
2026-08-17 08:36:01 +1000

    initial commit

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 provider "aws" {
   region = "ap-southeast-4"
 }
@@ -11,7 +14,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "tfstate"
+    bucket       = "tfstate-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an"
     key          = "${var.environment}.tfstate"
     encrypt      = true
     use_lockfile = true

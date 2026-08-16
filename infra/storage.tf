1c4c80b51a5b0453e790290461152bbfd5ca1531
odisfm <102568741+odisfm@users.noreply.github.com>
2026-08-17 08:32:01 +1000

    initial commit

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,6 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
 resource "aws_s3_bucket" "icons_bucket" {
   bucket = format("icons-${var.environment}-%s-%s-an", data.aws_caller_identity.current.account_id, data.aws_region.current.region)
   bucket_namespace = "account-regional"

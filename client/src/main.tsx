6f802cffa994d86c5e290698933d5d761a8670fd
Max Ivanovic <132530642+mivan50@users.noreply.github.com>
2026-09-13 13:54:49 +1000

    feat: add admin routing shell (#20)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,16 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import "./index.css"
 import App from "./App.tsx";
+import AdminLoginPage from "./pages/admin/AdminLoginPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+    },
+    {
+        path: "/admin-login",
+        element: <AdminLoginPage />,
     },
 ]);
 

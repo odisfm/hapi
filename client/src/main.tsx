cdb87a2eac3f025f60b47c54ea24d40ef334637b
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-16 14:18:02 +1000

    feat: configure router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,15 +4,22 @@ import { RouterProvider } from "react-router/dom";
 import "./index.css"
 import App from "./App.tsx";
 import AdminLoginPage from "./pages/admin/AdminLoginPage.tsx";
+import TestApi from "./components/TestApi.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-    },
-    {
-        path: "/admin-login",
-        element: <AdminLoginPage />,
+        children: [
+            {
+                index: true,
+                element: <TestApi />
+            },
+            {
+                path: "/admin-login",
+                element: <AdminLoginPage />
+            }
+        ]
     },
 ]);
 

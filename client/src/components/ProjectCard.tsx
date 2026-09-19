e31b39f5fef9a6a7ec016047dd5428f3dfe66544
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-19 20:11:20 +1000

    feat: slight UI adjustments

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -10,14 +10,14 @@ export default function ProjectCard({name, subtitle, iconUrl, slug}: ProjectCard
     return (
         <Link
             to={`/project/${encodeURIComponent(slug)}`}
-            className="flex w-full items-center gap-4 rounded-xl bg-section-grey p-4 text-black"
+            className="flex w-full items-center gap-4 rounded-2xl bg-section-grey px-4 pt-4 min-h-25 text-black"
         >
-            <div className="min-w-0">
-                <h3 className="font-copy line-clamp-2 text-[1rem] font-bold leading-[1.3] tracking-normal">
             <div className={`grid grid-rows-[12fr_6fr]`}>
                 <AppIcon uri={iconUrl} width={70}/>
                 <div></div>
             </div>
+            <div className="min-w-0 h-full grid grid-rows-[1fr_1fr] gap-1">
+                <h3 className="font-copy line-clamp-2 text-[1rem] font-normal leading-[1.2] tracking-normal mt-0.5">
                     {name}
                 </h3>
                 <p className="font-copy line-clamp-2 text-[0.7rem] font-medium uppercase leading-[1.3] tracking-normal">

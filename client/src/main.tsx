7dc15832e12a5502bb1510d6d1f74ee802a9ad81
Max Ivanovic <s4014458@student.rmit.edu.au>
2026-09-19 16:15:11 +1000

    feat: add home page
    feat: apply Museo and Helvetica Neue fonts to homepage
    
    Tidy homepage and project card code
    
    Add project card grid, Projects heading and View More button
    
    Add homepage hero section (banner, title, intro text)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,10 @@ import { RouterProvider } from "react-router/dom";
 import "./index.css"
 import App from "./App.tsx";
 import AdminLoginPage from "./pages/admin/AdminLoginPage.tsx";
-import TestApi from "./components/TestApi.tsx";
 import {AuthProvider} from "./contexts/auth/AuthProvider.tsx";
 import {AdminDashboard} from "./pages/admin/AdminDashboard.tsx";
 import ProjectPage from "./pages/ProjectPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
 import {ProjectEditor} from "./components/ProjectEditor/ProjectEditor.tsx";
 import {ProjectList} from "./components/ProjectList/ProjectList.tsx";
 import {ModalProvider} from "./contexts/modal/ModalProvider.tsx";
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <TestApi />
+                element: <HomePage />
             },
             {
                 path: "/admin-login",

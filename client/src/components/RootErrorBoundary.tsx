1e20caa8b2dc3570c1dabc01d2c4cd6b2f9f5dc7
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-25 20:12:31 +1000

    feat: add error boundary
    fixes #52

---
+++ client/src/components/RootErrorBoundary.tsx
@@ -0,0 +1,57 @@
+import {isRouteErrorResponse, Link, useRouteError} from "react-router";
+import App from "../App.tsx";
+
+export function RootErrorBoundary() {
+    const error = useRouteError()
+    if (isRouteErrorResponse(error)) {
+        return (
+            <App>
+                <div className={`w-80 mt-20`}>
+                    <h1
+                        className={`text-5xl font-normal mb-6`}
+                    >
+                        Error 404
+                    </h1>
+                    <p className={`text-2xl mb-12`}>
+                        This page wandered off.
+                        <br/>
+                        Let's take you back.
+                    </p>
+                    <Link
+                        to={"/"}
+                        className={`
+                        min-w-48 shrink-0 rounded-full bg-r-blue px-16 py-3 text-center text-sm 
+                        font-bold text-white font-copy cursor-pointer
+                        `}
+                    >
+                        HOME PAGE
+                    </Link>
+                </div>
+            </App>
+        )
+    } else {
+        return (
+            <div
+                className={`
+                w-[100dvw] h-[100dvh] bg-neutral-200 flex flex-col items-center justify-center
+                `}>
+                <div className={`flex flex-col items-center gap-2 mt-[-20%]`}>
+                    <h1
+                        className={`text-5xl font-normal mb-6`}
+                    >
+                        An error occurred
+                    </h1>
+                    <Link
+                        to={"/"}
+                        className={`
+                        min-w-48 shrink-0 rounded-full bg-r-blue px-16 py-3 text-center text-sm 
+                        font-bold text-white font-copy cursor-pointer
+                        `}
+                    >
+                        HOME PAGE
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+}
